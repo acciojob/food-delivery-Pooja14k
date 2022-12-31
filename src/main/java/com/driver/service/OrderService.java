0ccdfb4ceb6bb706ffeb502d7a0613ec13183ff9
Pooja Kale <99339372+Pooja14k@users.noreply.github.com>
2022-12-31 19:25:25 +0530

    Update OrderService.java

--- src/main/java/com/driver/service/OrderService.java
+++ src/main/java/com/driver/service/OrderService.java
@@ -1,17 +1,56 @@
-package com.driver.service;
+package com.driver.service.impl;
+
+import com.driver.io.entity.OrderEntity;
+import com.driver.io.repository.OrderRepository;
+import com.driver.service.OrderService;
+import com.driver.shared.dto.OrderDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+@Service
+public class OrderServiceImpl implements OrderService {
 
-import com.driver.shared.dto.OrderDto;
+    @Autowired
+    OrderRepository orderRepository;
+    @Override
+    public OrderDto createOrder(OrderDto orderID) {
+        OrderEntity orderEntity=OrderEntity.builder().orderId(orderID.getOrderId()).id(orderID.getId()).cost(orderID.getCost()).items(orderID.getItems()).userId(orderID.getUserId()).status(orderID.isStatus()).build();
+        orderRepository.save(orderEntity);
+        return OrderDto.builder().orderId(orderEntity.getOrderId()).id(orderEntity.getId()).cost(orderEntity.getCost()).items(orderEntity.getItems()).status(orderEntity.isStatus()).userId(orderEntity.getUserId()).build();
+    }
 
-/**
- * Handle exception cases for all methods which throw Exception
- */
-public interface OrderService {
+    @Override //Overriding
+    public OrderDto getOrderById(String orderID) throws Exception {
+        OrderEntity orderEntity=orderRepository.findByOrderId(orderID);
+        return OrderDto.builder().orderId(orderEntity.getOrderId()).id(orderEntity.getId()).cost(orderEntity.getCost()).items(orderEntity.getItems()).status(orderEntity.isStatus()).userId(orderEntity.getUserId()).build();
+    }
 
-	OrderDto createOrder(OrderDto order);
-	OrderDto getOrderById(String orderId) throws Exception;
-	OrderDto updateOrderDetails(String orderId, OrderDto order) throws Exception;
-	void deleteOrder(String orderId) throws Exception;
-	List<OrderDto> getOrders();
+    @Override
+    public OrderDto updateOrderDetails(String orderId, OrderDto order) throws Exception {
+        OrderEntity orderEntity=orderRepository.findByOrderId(orderId);
+        orderEntity.setOrderId(order.getOrderId());
+        orderEntity.setCost(order.getCost());
+        orderEntity.setId(order.getId());
+        orderEntity.setItems(order.getItems());
+        orderEntity.setStatus(order.isStatus());
+        orderEntity.setUserId(order.getUserId());
+        orderEntity=orderRepository.save(orderEntity);
+        return OrderDto.builder().orderId(orderEntity.getOrderId()).id(orderEntity.getId()).cost(orderEntity.getCost()).items(orderEntity.getItems()).status(orderEntity.isStatus()).userId(orderEntity.getUserId()).build();
+    }
+
+    @Override
+    public void deleteOrder(String order) throws Exception {
+        orderRepository.delete(orderRepository.findByOrderId(order));
+    }
+
+    @Override
+    public List<OrderDto> getOrders() {
+        List<OrderDto> orderDto=null;
+        List<OrderEntity> orderEntities= (List<OrderEntity>) orderRepository.findAll();
+        for(OrderEntity orderEntity:orderEntities){
+            orderDto.add(OrderDto.builder().orderId(orderEntity.getOrderId()).id(orderEntity.getId()).cost(orderEntity.getCost()).items(orderEntity.getItems()).status(orderEntity.isStatus()).userId(orderEntity.getUserId()).build());
+        }
+        return orderDto;
+    }
 }
